0eab027ae5814710e3388f84701beb5314c5d085
o-filippov <olexiy.filippov@gmail.com>
2014-06-04 22:20:03 +0300

    fixed problem with group modification

--- addressbook-selenium-tests/src/com/example/fw/GroupHelper.java
+++ addressbook-selenium-tests/src/com/example/fw/GroupHelper.java
@@ -37,10 +37,11 @@ public class GroupHelper extends HelperBase {
 	}
 
 	private void selectGroupByIndex(int index) {
-		click(By.xpath("//input[@name='selected[]'][" + (index + 1) + "]"));
+		click(By.xpath("//input[@name='selected[]'][" + (index+1) + "]"));
 	}
 
 	public void initGroupModification(int index) {
+		selectGroupByIndex(index);
 		click(By.name("edit"));
 	}
 
