e3a9295b24709e0978d5e1b09618c6f82df4c5da
o-filippov <olexiy.filippov@gmail.com>
2014-06-07 19:17:46 +0300

    group modification tests with proper checks were done

--- addressbook-selenium-tests/src/com/example/tests/GroupCreationTests.java
+++ addressbook-selenium-tests/src/com/example/tests/GroupCreationTests.java
@@ -1,14 +1,16 @@
 package com.example.tests;
 
+import static org.testng.Assert.assertEquals;
+
 import java.util.Collections;
 import java.util.List;
-import static org.testng.Assert.assertEquals;
+
 import org.testng.annotations.Test;
 
 public class GroupCreationTests extends TestBase {
 	
-  @Test
-  public void testNonEmptyGroupCreation() throws Exception {
+  @Test(dataProvider = "randomValidGroupGenerator")
+  public void testGroupCreationWithValidData(GroupData group) throws Exception {
     app.getNavigationHelper().openMainPage();
     app.getNavigationHelper().gotoGroupsPage();
     
@@ -17,10 +19,6 @@ public class GroupCreationTests extends TestBase {
     
     // actions    
     app.getGroupHelper().initGroupCreation();
-	GroupData group = new GroupData();
-	group.name = "aaa";
-	group.header = "header 1";
-	group.footer = "footer 1";
 	app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupCreation();
     app.getGroupHelper().returnToGroupsPage();
@@ -35,28 +33,4 @@ public class GroupCreationTests extends TestBase {
     
   }
   
- @Test
-  public void testEmptyGroupCreation() throws Exception {
-    app.getNavigationHelper().openMainPage();
-    app.getNavigationHelper().gotoGroupsPage();
-    
-    // save old state
-    List<GroupData> oldList = app.getGroupHelper().getGroups();
-   
-    //actions 
-	GroupData group = new GroupData("", "", "");
-    app.getGroupHelper().initGroupCreation();
-	app.getGroupHelper().fillGroupForm(group);
-    app.getGroupHelper().submitGroupCreation();
-    app.getGroupHelper().returnToGroupsPage();
-    
-    // save new state
-    List<GroupData> newList = app.getGroupHelper().getGroups();
-    
-    //compare states
-    oldList.add(group);
-    Collections.sort(oldList);
-    assertEquals(newList, oldList);
-  
-  }
 }
