89a8ed69551f29c85b6dbe9d18d31261c5ec7324
o-filippov <olexiy.filippov@gmail.com>
2014-06-10 22:58:47 +0300

    Proper checks on contact operations are done
    This commit covers the Home Work 6.
    Some git conflicts were resolved. I hope actins taken were correct :)

--- addressbook-selenium-tests/src/com/example/tests/ContactData.java
+++ addressbook-selenium-tests/src/com/example/tests/ContactData.java
@@ -55,24 +55,4 @@ public class ContactData implements Comparable<ContactData> {
 		return this.last_name.toLowerCase().compareTo(other.last_name.toLowerCase());
 	}
 	
-	/* public ContactData(String first_name, String last_name, String address,
-			String home_phone, String mobile_phone, String work_phone,
-			String email, String email_2, String birth_day, String birth_month,
-			String birth_year, String group, String address_2,
-			String home_phone_2) {
-		this.first_name = first_name;
-		this.last_name = last_name;
-		this.address = address;
-		this.home_phone = home_phone;
-		this.mobile_phone = mobile_phone;
-		this.work_phone = work_phone;
-		this.email = email;
-		this.email_2 = email_2;
-		this.birth_day = birth_day;
-		this.birth_month = birth_month;
-		this.birth_year = birth_year;
-		this.group = group;
-		this.address_2 = address_2;
-		this.home_phone_2 = home_phone_2;
-	} */
 }
