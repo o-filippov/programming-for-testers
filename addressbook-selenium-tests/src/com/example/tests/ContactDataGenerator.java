b16e03cec3e6127513236c3b54f0794a145a5fbb
o-filippov <olexiy.filippov@gmail.com>
2014-07-07 21:37:26 +0300

    Test data generators for groups and contacts - HT11

---
+++ addressbook-selenium-tests/src/com/example/tests/ContactDataGenerator.java
@@ -0,0 +1,132 @@
+package com.example.tests;
+
+import static com.example.tests.GroupDataGenerator.generateRandomString;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import com.thoughtworks.xstream.XStream;
+
+public class ContactDataGenerator {
+
+	public static void main(String[] args) throws IOException {
+		if (args.length < 3) {
+			System.out.println("Please specify parameters: <test data amount> <file> <format>");
+			return;
+		}
+	
+		int amount = Integer.parseInt(args[0]);
+		File file = new File(args[1]);
+		String format = args[2];
+		
+		if (file.exists()) {
+			System.out.println("File '" + file + "' exists, please remove it manually");
+			return;	
+		}
+		
+		List<ContactData> contacts = generateRandomContacts(amount);
+		if ("csv".equals(format)) {
+			saveContactsToCsvFile(contacts, file);
+		} else if ("xml".equals(format)) {
+			saveContactsToXmlFile(contacts, file);
+		} else {
+			System.out.println("Unknown format " + format +". Please, use csv or xml");
+			return;
+		}
+	}
+	
+	private static void saveContactsToXmlFile(List<ContactData> contacts, File file) throws IOException {
+		XStream xstream = new XStream();
+		xstream.alias("contact", ContactData.class);
+		String xml = xstream.toXML(contacts);	
+		FileWriter writer = new FileWriter(file);
+		writer.write(xml);
+		writer.close();
+	}
+	
+	private static void saveContactsToCsvFile(List<ContactData> contacts, File file) throws IOException {
+		FileWriter writer = new FileWriter(file);
+		for (ContactData contact : contacts) {
+			writer.write(contact.getFirstName() + "," + 
+							contact.getLastName() + "," + 
+							contact.getAddress() + "," + 
+							contact.getHomePhone() + "," + 
+							contact.getMobilePhone() + "," + 
+							contact.getWorkPhone() + "," + 
+							contact.getEmail() + "," + 
+							contact.getEmail2() + "," + 
+							contact.getBirthDay() + "," + 
+							contact.getBirthMonth() + "," + 
+							contact.getBirthYear() + "," + 
+							contact.getAddress() + "," + 
+							contact.getAddress2() + "," + 
+							contact.getHomePhone2() + ",!" + "\n");
+		}
+		writer.close();
+	}
+	
+	public static List<ContactData> generateRandomContacts(int amount) {
+		List<ContactData> list = new ArrayList<ContactData>();
+		for (int i = 0; i < amount; i++) {
+			ContactData contact = new ContactData()
+			.withFirstName(generateRandomString())
+			.withLastName(generateRandomString())
+			.withAddress(generateRandomString())
+			.withHomePhone(generateRandomNumber())
+			.withMobilePhone(generateRandomNumber())
+			.withWorkPhone(generateRandomNumber())
+			.withEmail(generateRandomString())
+			.withEmail2(generateRandomString())
+			.withBirthDay(generateRandomDay())
+			.withBirthMonth(generateRandomMonth())
+			.withBirthYear(generateRandomYear())
+			.withGroup("[none]")
+			.withAddress2(generateRandomString())
+			.withHomePhone2(generateRandomNumber());
+			list.add(contact);
+		}
+		return list;
+	}
+	
+	public static String generateRandomYear() {
+		Random rnd = new Random();
+		if (rnd.nextInt(3) == 0) {
+			return "";
+		} else {
+			return String.valueOf(rnd.nextInt(110) + 1900);
+		}
+	}
+	
+	public static String generateRandomDay() {
+		Random rnd = new Random();
+		if (rnd.nextInt(3) == 0) {
+			return "-";
+		} else {
+			return String.valueOf(rnd.nextInt(29) + 1);
+		}
+	}
+	
+	public static String generateRandomNumber() {
+		Random rnd = new Random();
+		if (rnd.nextInt(3) == 0) {
+			return "";
+		} else {
+			return String.valueOf(Math.abs(rnd.nextInt()));
+		}
+	}
+	
+	public static String generateRandomMonth() {
+		String[] twelvemonth = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+		Random rnd = new Random();
+		if (rnd.nextInt(3) == 0) {
+			return "-";
+		} else {
+			int index = new Random().nextInt(12);
+			return twelvemonth[index];
+		}
+	}
+}
