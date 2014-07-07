0b0e461727e6d516353bb70f566f1976406fb884
o-filippov <olexiy.filippov@gmail.com>
2014-07-07 21:39:21 +0300

    Test data generators for groups and contacts - HT11
    HT11

--- addressbook-selenium-tests/src/com/example/tests/TestBase.java
+++ addressbook-selenium-tests/src/com/example/tests/TestBase.java
@@ -1,9 +1,11 @@
 package com.example.tests;
 
+import static com.example.tests.ContactDataGenerator.generateRandomContacts;
+import static com.example.tests.GroupDataGenerator.generateRandomGroups;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Random;
 
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -27,89 +29,27 @@ public class TestBase {
 
 	@DataProvider
 	public Iterator<Object[]> randomValidGroupGenerator() {
-		List<Object[]> list = new ArrayList<Object[]>();
-		for (int i = 0; i < 5; i++) {
-			GroupData group = new GroupData()
-			 .withName(generateRandomString())
-			 .withHeader(generateRandomString())
-			 .withFooter(generateRandomString());
-			list.add(new Object[]{group});
-		}
-		// ....		
-		return list.iterator();
+		return wrapGroupsForDataProvider(generateRandomGroups(5)).iterator();
 	}
 	
-	public String generateRandomString() {
-		Random rnd = new Random();
-		if (rnd.nextInt(3) == 0) {
-			String string = new String();
-			string = "";
-			return string;
-		} else {
-			String string = new String();
-			string = "test" + rnd.nextInt();
-			return string; 
+	public static List<Object[]> wrapGroupsForDataProvider(List<GroupData> groups) {
+		List<Object[]> list = new ArrayList<Object[]>();
+		for (GroupData group : groups) {
+			list.add(new Object[]{group});
 		}
+		return list;
 	}
 	
 	@DataProvider
 	public Iterator<Object[]> randomValidContactGenerator() {
+		return wrapContactsForDataProvider(generateRandomContacts(5)).iterator();
+	}
+
+	private List<Object[]> wrapContactsForDataProvider(List<ContactData> contacts) {
 		List<Object[]> list = new ArrayList<Object[]>();
-		for (int i = 0; i < 5; i++) {
-			ContactData contact = new ContactData()
-			.withFirstName(generateRandomString())
-			.withLastName(generateRandomString())
-			.withAddress(generateRandomString())
-			.withHomePhone(generateRandomNumber())
-			.withMobilePhone(generateRandomNumber())
-			.withWorkPhone(generateRandomNumber())
-			.withEmail(generateRandomString())
-			.withEmail2(generateRandomString())
-			.withBirthDay(generateRandomDay())
-			.withBirthMonth(generateRandomMonth())
-			.withBirthYear(generateRandomYear())
-			.withGroup("[none]")
-			.withAddress2(generateRandomString())
-			.withHomePhone2(generateRandomNumber());
+		for (ContactData contact : contacts) {
 			list.add(new Object[]{contact});
 		}
-		return list.iterator();
-	}
-	
-	public String generateRandomYear() {
-		Random rnd = new Random();
-		if (rnd.nextInt(3) == 0) {
-			return "";
-		} else {
-			return String.valueOf(rnd.nextInt(110) + 1900);
-		}
-	}
-	
-	public String generateRandomDay() {
-		Random rnd = new Random();
-		if (rnd.nextInt(3) == 0) {
-			return "-";
-		} else {
-			return String.valueOf(rnd.nextInt(29) + 1);
-		}
-	}
-	
-	public String generateRandomNumber() {
-		Random rnd = new Random();
-		if (rnd.nextInt(3) == 0) {
-			return "";
-		} else {
-			return String.valueOf(Math.abs(rnd.nextInt()));
-		}
-	}
-	
-	public String generateRandomMonth() {
-		String[] twelvemonth = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
-		Random rnd = new Random();
-		if (rnd.nextInt(3) == 0) {
-			return "-";
-		} else {
-			return twelvemonth[new Random().nextInt(12)];
-		}
-	}
+		return list;
+	}	
 }
